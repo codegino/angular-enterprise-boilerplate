99e4d05295457a00320823064100c61417c1487b
dbsitt <carloginocatapang@gmail.com>
2019-10-23 20:30:37 +0800

    Added projects
    Added ngrx dependencies

---
+++ projects/scheduler/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'schdlr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'scheduler';
+}
